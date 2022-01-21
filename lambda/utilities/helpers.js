2be4f87551b0b52a82fb5daa1126ad307342e5db
William Valdez <williamvaldez@outlook.com>
2022-01-21 23:52:17 +0100

    First commit

---
+++ lambda/utilities/helpers.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IsType = exports.IsIntent = void 0;
+const constants_1 = require("./constants");
+/**
+ * Checks if the request matches any of the given intents.
+ *
+ * @param handlerInput
+ * @param intents
+ */
+function IsIntent(handlerInput, ...intents) {
+    if (handlerInput.requestEnvelope.request.type === constants_1.RequestTypes.Intent) {
+        for (let i = 0; i < intents.length; i++) {
+            if (handlerInput.requestEnvelope.request.intent.name === intents[i]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+exports.IsIntent = IsIntent;
+/**
+ * Checks if the request matches any of the given types.
+ *
+ * @param handlerInput
+ * @param types
+ */
+function IsType(handlerInput, ...types) {
+    for (let i = 0; i < types.length; i++) {
+        if (handlerInput.requestEnvelope.request.type === types[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+exports.IsType = IsType;
---
+++ lambda/utilities/strings.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.strings = void 0;
+const constants_1 = require("./constants");
+exports.strings = {
+    [constants_1.LocaleTypes.esES]: {
+        translation: {
+            SKILL_NAME: 'Hello world',
+            WELCOME_MSG: 'Bienvenido, puedes decir Hola o Ayuda. Cual prefieres?',
+            HELLO_MSG: 'Hola Mundo!',
+            HELP_MSG: 'Puedes decirme hola. Cómo te puedo ayudar?',
+            GOODBYE_MSG: 'Hasta luego!',
+            REFLECTOR_MSG: 'Acabas de activar {{intentName}}',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+            ERROR_MSG: 'Lo siento, ha habido un error. Por favor inténtalo otra vez.',
+        },
+    },
+};
